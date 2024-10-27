63810284b8fb1031d0b301ec6fcb149fc12e0056
Kristina Faye <hellokristinafaye@gmail.com>
2024-10-27 14:39:33 -0700

    yay! the weather icon updates based on the specific city's weather report!

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ async function checkWeather(city) {
         weatherIcon.src = "images/clouds.png";
     } else if (data.weather[0].main == "Clear") {
         weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
     }
 }
 
