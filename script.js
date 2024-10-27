a1a1bf46f45fa6d2391683003e6051623dd5fc1e
Kristina Faye <hellokristinafaye@gmail.com>
2024-10-27 13:37:16 -0700

    SUCCESS the api data logs in the console on the web page!  I'm not able to node script.js it tho, so that feels weird?  But the browswer console is how the tutorial does it so hey, a win is a win

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const apiKey = "2bd5559124f13def23addea6864b8f2c";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=seattle";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
